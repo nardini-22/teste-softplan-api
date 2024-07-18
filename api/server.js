057eeecd4c5b4d0e19a1561e34b373e75e288708
nardini-22 <arthur.nardini03@gmail.com>
2024-07-18 00:26:47 -0300

    fix: server now accept other methods

--- api/server.js
+++ api/server.js
@@ -2,7 +2,12 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create()
 
-const router = jsonServer.router('db.json')
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 const middlewares = jsonServer.defaults()
 
