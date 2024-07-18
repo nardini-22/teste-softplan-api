1ae6a441372912db65004fea944bceaefe1538d3
nardini-22 <arthur.nardini03@gmail.com>
2024-07-18 10:46:42 -0300

    feat: add json-server-auth

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const jsonServer = require('json-server');
-
+const auth = require("json-server-auth");
 const server = jsonServer.create()
 
 const fs = require('fs')
@@ -11,10 +11,13 @@ const router = jsonServer.router(db)
 
 const middlewares = jsonServer.defaults()
 
+server.db = router.db
+
 server.use(middlewares)
 server.use(jsonServer.rewriter({
 	'/api/*': '/$1',
 }))
+server.use(auth)
 server.use(router)
 server.listen(3000, () => {
 	console.log('JSON Server is running')
