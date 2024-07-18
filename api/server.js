4c32388ac55a200b427d37212a17aadd72a43071
nardini-22 <arthur.nardini03@gmail.com>
2024-07-18 00:11:36 -0300

    fix: try to deploy

--- api/server.js
+++ api/server.js
@@ -7,7 +7,9 @@ const router = jsonServer.router('api/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-
+server.use(jsonServer.rewriter({
+	'/api/*': '/$1',
+}))
 server.use(router)
 server.listen(3000, () => {
 	console.log('JSON Server is running')
