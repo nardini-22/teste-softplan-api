605b93687400df97810f13042909f401068a44ca
nardini-22 <arthur.nardini03@gmail.com>
2024-07-17 23:54:46 -0300

    initial api build

---
+++ api/server.js
@@ -0,0 +1,16 @@
+import jsonServer from "json-server"
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('api/db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(router)
+server.listen(40, () => {
+	console.log('JSON Server is running')
+})
+
+export default server
