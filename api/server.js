27b976e54066198bde0d59818ec932e0eafbbab0
nardini-22 <arthur.nardini03@gmail.com>
2024-07-18 00:01:19 -0300

    fix: try to deploy

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 
 server.use(router)
-server.listen(40, () => {
+server.listen(3000, () => {
 	console.log('JSON Server is running')
 })
 
